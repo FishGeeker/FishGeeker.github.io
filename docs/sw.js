30862262db090238952fa1875280b1a746afb22e
FishGeeker <FishGeeker@users.noreply.github.com>
2021-05-01 14:59:54 +0000

    🧪 Deploy at Sat May  1 14:59:54 UTC 2021

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210501.1642';
+const cacheName = 'chirpy-20210501.1659';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
